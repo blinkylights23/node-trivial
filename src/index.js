d927433f294c73dfca4910f49fe12dd8a4718871
Paul Smith <paul@blinkylights.org>
2017-02-06 09:38:42 -0500

    Improved category method

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const trivia = require('./trivia');
 module.exports = {
   all: trivia,
   random: () => trivia[Math.floor(Math.random() * trivia.length)],
-  categories: new Set(trivia.map(i => i.category.title))
+  categories: new Set(trivia.map(i => { return { id: i.category_id, title: i.category.title } }))
 }
